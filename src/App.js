49059066346774fb32b2054a923ec75e0e3b536c
sgc1996 <anythingceylon96@gmail.com>
2024-07-22 10:22:59 +0530

    Tailwind config

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 const App = () => {
   return (
-    <div>
-
-    </div>
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
   );
 }
 
